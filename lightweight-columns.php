a3545e8e5f3d57e7a72774da7c2210516d2f9066
K. Belardinelli <k@kangabell.co>
2015-02-20 17:18:35 -0500

    bring in initial code

--- lightweight-columns.php
+++ lightweight-columns.php
@@ -12,6 +12,46 @@ Version: 1.0
 Author URI: http://kangabell.co
 */
 
+/* HALF COLUMNS */
+    function half_first_function( $atts, $content = null ) {
+        return "<div class='columns'><div class='sixcol first'>".do_shortcode($content)."</div>";
+    }
+    add_shortcode( 'half_first', 'half_first_function' );
+
+    function half_last_function( $atts, $content = null ) {
+        return "<div class='sixcol last'>".do_shortcode($content)."</div></div>";
+    }
+    add_shortcode( 'half_last', 'half_last_function' );
+
+/* THIRDS COLUMNS */
+
+    /* 2/3 left, 1/3 right */
+        function twothirds_first_function( $atts, $content = null ) {
+            return "<div class='columns'><div class='eightcol first'>".do_shortcode($content)."</div>";
+        }
+        add_shortcode( 'twothirds_first', 'twothirds_first_function' );
+
+        function onethird_last_function( $atts, $content = null ) {
+            return "<div class='fourcol last'>".do_shortcode($content)."</div></div>";
+        }
+        add_shortcode( 'onethird_last', 'onethird_last_function' );
+
+    /* 1/3 left, 2/3 right */
+        function onethird_first_function( $atts, $content = null ) {
+            return "<div class='columns'><div class='fourcol first'>".do_shortcode($content)."</div>";
+        }
+        add_shortcode( 'onethird_first', 'onethird_first_function' );
+
+        function twothirds_last_function( $atts, $content = null ) {
+            return "<div class='eightcol last'>".do_shortcode($content)."</div></div>";
+        }
+        add_shortcode( 'twothirds_last', 'twothirds_last_function' );
+
+/* THREE FLOWING COLUMNS */
+    function threecolumns_function( $atts, $content = null ) {
+        return "<div class='columns'><div class='threecolumns'>".do_shortcode($content)."</div></div>";
+    }
+    add_shortcode( 'threecolumns', 'threecolumns_function' );
 
 
 ?>
