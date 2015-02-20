51b39f1835bd8a8274ec6a947720b26ad84b8ebe
K. Belardinelli <k@kangabell.co>
2015-02-20 17:03:01 -0500

    create plugin

---
+++ lightweight-columns.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package Lightweight_Columns
+ * @version 1.0
+ */
+/*
+Plugin Name: Lightweight Columns
+Plugin URI: http://github.com/kangabell/lightweight-columns
+Description: Shortcodes for simple, responsive columns. Includes thirds, halves, and three-column flowing text.
+Author: Kay Belardinelli
+Version: 1.0
+Author URI: http://kangabell.co
+*/
+
+
+
+?>
