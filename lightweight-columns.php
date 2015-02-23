1757317199ce5db2927dec3911d465d89a973d51
K. Belardinelli <k@kangabell.co>
2015-02-22 21:00:47 -0500

    add unique class to avoid theme/plugin conflicts

--- lightweight-columns.php
+++ lightweight-columns.php
@@ -20,15 +20,14 @@ function lightweight_columns_scripts() {
 
 add_action( 'wp_enqueue_scripts', 'lightweight_columns_scripts');
 
-
 /* HALF COLUMNS */
     function half_first_function( $atts, $content = null ) {
-        return "<div class='columns'><div class='sixcol first'>".do_shortcode($content)."</div>";
+        return "<div class='lightweight-columns'><div class='first sixcol'>".do_shortcode($content)."</div>";
     }
     add_shortcode( 'half_first', 'half_first_function' );
 
     function half_last_function( $atts, $content = null ) {
-        return "<div class='sixcol last'>".do_shortcode($content)."</div></div>";
+        return "<div class='last sixcol'>".do_shortcode($content)."</div></div>";
     }
     add_shortcode( 'half_last', 'half_last_function' );
 
@@ -36,29 +35,29 @@ add_action( 'wp_enqueue_scripts', 'lightweight_columns_scripts');
 
     /* 2/3 left, 1/3 right */
         function twothirds_first_function( $atts, $content = null ) {
-            return "<div class='columns'><div class='eightcol first'>".do_shortcode($content)."</div>";
+            return "<div class='lightweight-columns'><div class='first eightcol'>".do_shortcode($content)."</div>";
         }
         add_shortcode( 'twothirds_first', 'twothirds_first_function' );
 
         function onethird_last_function( $atts, $content = null ) {
-            return "<div class='fourcol last'>".do_shortcode($content)."</div></div>";
+            return "<div class='last fourcol'>".do_shortcode($content)."</div></div>";
         }
         add_shortcode( 'onethird_last', 'onethird_last_function' );
 
     /* 1/3 left, 2/3 right */
         function onethird_first_function( $atts, $content = null ) {
-            return "<div class='columns'><div class='fourcol first'>".do_shortcode($content)."</div>";
+            return "<div class='lightweight-columns'><div class='first fourcol'>".do_shortcode($content)."</div>";
         }
         add_shortcode( 'onethird_first', 'onethird_first_function' );
 
         function twothirds_last_function( $atts, $content = null ) {
-            return "<div class='eightcol last'>".do_shortcode($content)."</div></div>";
+            return "<div class='last eightcol'>".do_shortcode($content)."</div></div>";
         }
         add_shortcode( 'twothirds_last', 'twothirds_last_function' );
 
 /* THREE FLOWING COLUMNS */
     function threecolumns_function( $atts, $content = null ) {
-        return "<div class='columns'><div class='threecolumns'>".do_shortcode($content)."</div></div>";
+        return "<div class='lightweight-columns'><div class='threecolumns'>".do_shortcode($content)."</div></div>";
     }
     add_shortcode( 'threecolumns', 'threecolumns_function' );
 
