1a75d23ae2d1158ba9f189bcd356454be8328e91
K Belardinelli <k@kangabell.co>
2017-10-22 18:50:30 -0400

    basic conversion from floats to flexbox

--- lightweight-columns.php
+++ lightweight-columns.php
@@ -16,57 +16,28 @@ Author URI: http://kangabell.co
 /* LOAD STYLESHEET */
 add_action( 'wp_enqueue_scripts', 'lwc_scripts');
 
-/* HALF COLUMNS */
-add_shortcode( 'half_first', 'lwc_half_first' );
-add_shortcode( 'half_last', 'lwc_half_last' );
-
-/* THIRDS COLUMNS */
-
-/* 2/3 left, 1/3 right */
-add_shortcode( 'twothirds_first', 'lwc_twothirds_first' );
-add_shortcode( 'onethird_last', 'lwc_onethird_last' );
-
-/* 1/3 left, 2/3 right */
-add_shortcode( 'onethird_first', 'lwc_onethird_first' );
-add_shortcode( 'twothirds_last', 'lwc_twothirds_last' );
-
-/* THREE FLOWING COLUMNS */
-add_shortcode( 'threecolumns', 'lwc_threecolumns' );
+add_shortcode( 'lwc', 'lwc_wrapper' );
+add_shortcode( 'column', 'lwc_column' );
 
 
 /*** FUNCTIONS ***/
 
 function lwc_scripts() {
-    wp_enqueue_style( 'lwc-css', plugins_url( 'style.min.css', __FILE__ ) );
+    wp_enqueue_style( 'lwc-css', plugins_url( 'style.css', __FILE__ ) );
     wp_enqueue_script('lwc-js', plugins_url( 'scripts.js', __FILE__ ), array('jquery'), null, true);
 }
 
-function lwc_half_first( $atts, $content = null ) {
-    return "<div class='lwc'><div class='first sixcol'>".do_shortcode($content)."</div>";
+function lwc_wrapper( $atts, $content = null ) {
+
+    extract( shortcode_atts( array(
+        'sidebar' => '',
+    ), $atts ) );
+    
+    return "<div class='lwc " . esc_attr($sidebar) . "'>" . do_shortcode($content) . "</div>";
 }
 
-function lwc_half_last( $atts, $content = null ) {
-    return "<div class='last sixcol'>".do_shortcode($content)."</div></div>";
-}
-
-function lwc_twothirds_first( $atts, $content = null ) {
-    return "<div class='lwc'><div class='first eightcol'>".do_shortcode($content)."</div>";
-}
-
-function lwc_onethird_last( $atts, $content = null ) {
-    return "<div class='last fourcol'>".do_shortcode($content)."</div></div>";
-}
-
-function lwc_onethird_first( $atts, $content = null ) {
-    return "<div class='lwc'><div class='first fourcol'>".do_shortcode($content)."</div>";
-}
-
-function lwc_twothirds_last( $atts, $content = null ) {
-    return "<div class='last eightcol'>".do_shortcode($content)."</div></div>";
-}
-
-function lwc_threecolumns( $atts, $content = null ) {
-    return "<div class='lwc'><div class='threecolumns'>".do_shortcode($content)."</div></div>";
+function lwc_column( $atts, $content = null ) {
+    return "<div>" . do_shortcode($content) . "</div>";
 }
     
 
